b5122dc908fe7f5adb370903eb6d0a77930843ca
liyubao <lyb@megvii.com>
2021-07-15 16:17:02 -0400

    fix: init

---
+++ client/client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:12346", "http service address")
+
+func main() {
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	var dialer *websocket.Dialer
+
+	conn, _, err := dialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//go timeWriter(conn)
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			return
+		}
+
+		fmt.Printf("received: %s\n", message)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module gs_test
+
+go 1.16
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/websocket v1.4.2
+	github.com/satori/go.uuid v1.2.0
+)
